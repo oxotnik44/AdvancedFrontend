7c23527dfca29a995fd404bd08eee72eb334c953
oxotnik44 <artemnovikov1969@gmail.com>
2025-04-30 22:16:34 +0700

    fix jest

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -1,10 +1,5 @@
 import path from 'path';
-import { fileURLToPath } from 'url';
 
-// получаем полный путь к текущему файлу
-const __filename = fileURLToPath(import.meta.url);
-// получаем директорию этого файла
-const __dirname = path.dirname(__filename);
 export default {
   clearMocks: true,
   testEnvironment: 'jsdom',
